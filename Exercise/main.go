eae4b0e8bf6530e63422324f87023e026cabafa1
akashranjan2903 <akashkr2903@gmail.com>
2023-01-06 19:31:19 +0530

    test

---
+++ Exercise/main.go
@@ -0,0 +1,106 @@
+package main
+
+func main() {
+
+	// practice.Checkpalindrome(101)
+	// game.Systemnum()
+	// game.Guess()
+	// practice.Play()
+	// practice.Name()
+
+	// var arr = []string{"a", "b", "c", "a", "b"}
+	// var mapValue = practice.Frequency(arr)
+	// fmt.Println(mapValue)
+	// var map1 = map[string]int{
+	// 	"a": 1, "c": 2, "b": 3,
+	// }
+	// var slice = practice.Sortedslice(map1)
+	// fmt.Printf("slice:%v\n", slice)
+	// var map2 = map[string]int{
+	// 	"a": 1, "b": 2, "c": 3,
+	// }
+	// fmt.Printf("New Map:%v\n", practice.Newmap(map2))
+	// newSlice := practice.Mapslice(map2, []string{"a", "b", "d"})
+	// fmt.Printf("New Slice:%v\n", newSlice)
+	// fmt.Printf("New Reversed Map:%v\n", practice.Reversedmap(map2))
+
+	// New Ques
+	slicee := Practice.Slicetest([]int{1, 2, 4, 4, 8, 3, 3, 3})
+	// slice := exercise1.Slice([]int{1, 2, 4, 4, 8, 3, 3, 3})
+	// fmt.Printf("New Slice with even number :%v\n", slice)
+
+	// slice1 := exercise1.Sortedslice([]string{"zebra", "monkey", "aardvark"})
+	// fmt.Printf("New sorted Slice:%v\n", slice1)
+
+	// newMap := exercise1.Alphabeticalfrequency("Hello")
+	// fmt.Printf("Frequency count of each character is %v\n", newMap)
+
+	// largestNo, secondLargestNo, sum, avg := exercise1.SumAvgLargest([]int{1, 2, 3, 4, 5})
+	// fmt.Printf("Largest No is :%d and second largest number is : %d and sum is: %d and avg is: %d\n", largestNo, secondLargestNo, sum, avg)
+
+	// slice2 := exercise1.Slicewithmaxletter([]string{"cat", "dog", "elephant", "lion"})
+	// fmt.Printf("Slice with more than 5 character %v\n", slice2)
+
+	// str := exercise1.Constantstr("hello")
+	// fmt.Printf("String with No Vowels:%s\n", str)
+
+	// slice3 := exercise1.Removeduplicate([]int{1, 2, 3, 2, 4, 5, 5})
+	// fmt.Printf("Slice with no duplicates :%v\n", slice3)
+
+	// check := exercise1.MatchString([]string{"cat", "dog", "elephant"}, "dog")
+	// fmt.Printf("String present in slice or nots: %t\n", check)
+
+	// occurence := exercise1.FirstOccurenceint([]int{1, 2, 3, 2, 4, 5}, 2)
+	// fmt.Printf("First Occurence of the given number at Index is:%d\n", occurence)
+
+	// totaloccurence := exercise1.TotalOccurence([]int{1, 2, 3, 2, 4, 5}, 2)
+	// fmt.Printf("Total Occurence of the given number at Index is:%d\n", totaloccurence)
+
+	// newSlice4 := exercise1.ReverseSlice([]int{1, 2, 3, 4, 5})
+	// fmt.Printf("Reversed slice is :%v\n", newSlice4)
+
+	// check1 := exercise1.CheckSorted([]int{1, 2, 3, 4, 5})
+	// fmt.Printf("Check the slice is sorted or not:%v\n", check1)
+
+	// stroccurence := exercise1.FirstOccurencestr([]string{"cat", "dog", "elephant"}, "dog")
+	// fmt.Printf("First Occurence of the given string at Index is:%d\n", stroccurence)
+
+	// slice4 := exercise1.TakestrFromletter([]string{"cat", "dog", "elephant"}, "e")
+	// fmt.Printf("All words start from the given letter is :%v\n", slice4)
+
+	// slice5 := exercise1.Checkpalindromestr([]string{"racecar", "level", "hello"})
+	// fmt.Printf("New Checked Palindrome Slice for string  is : %v\n", slice5)
+
+	// str2 := exercise1.ReversestrArray("This is a test")
+	// fmt.Printf("New Reversed words of String is: %s\n", str2)
+
+	// slice6 := exercise1.Prime([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	// fmt.Printf("New slice with prime number is: %v\n", slice6)
+
+	// slice7 := exercise1.Checkdivisibility([]int{1, 2, 3, 4, 5}, 2)
+	// fmt.Printf("New Slice which is divisible by given number is: %v\n", slice7)
+
+	// slice8 := exercise1.Checknumispreset([]int{1, 2, 3, 4, 5}, 3)
+	// fmt.Printf("New Slice which is divisible by given number is: %v\n", slice8)
+
+	// slice9 := exercise1.Slicewithminletter([]string{"cat", "dog", "elephant", "lion"}, 4)
+	// fmt.Printf(" Array with min letter present is:%v\n", slice9)
+
+	// str3 := "This is a test"
+	// newrune := []rune(str3)
+	// str3 = string(exercise1.Reverserunestr(newrune))
+	// fmt.Printf("Reverse String is : %s\n", str3)
+
+	// slice10 := exercise1.Checkletterpresent([]string{"cat", "dog", "elephant"}, "e")
+	// fmt.Printf("Slice with given letter is present: %v\n", slice10)
+
+	// check2 := exercise1.Lcm([]int{2, 3, 5})
+	// fmt.Printf("Lcm is :%v\n", check2)
+
+	// slice11 := exercise1.Numberpalindrome([]int{1, 11, 121, 12321})
+	// fmt.Printf("Slice with all Palindrome is: %v\n", slice11)
+
+	// check3 := exercise1.CheckdescendingSorted([]int{5, 4, 3, 2, 1})
+	// fmt.Printf("Check the slice is sorted or not:%v\n", check3)
+
+}
